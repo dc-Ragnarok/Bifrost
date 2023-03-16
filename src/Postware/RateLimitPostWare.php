65c42bb8696146ee7fcc689bbfc1709a300806d0
Command_String <bmxmikey2@gmail.com>
2023-03-16 09:53:23 +0000

    Ratelimitpostware started

--- src/Postware/RateLimitPostWare.php
+++ src/Postware/RateLimitPostWare.php
@@ -2,12 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Ragnarok\Bifrost;
+namespace Ragnarok\Bifrost\Postware;
+
+use Ragnarok\Bifrost\Response;
 
 class RateLimitPostWare implements PostwareInterface
 {
-    public function handle(Request $request, callable $next)
+    public function handle(Response $response, callable $next)
     {
-        $next($request);
+        $next($response);
     }
 }
